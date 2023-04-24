cc8004c68bc5df335a2f472ddb1335bb7b68d4b1
Armanur Rahman <mubeenarman19@gmail.com>
2023-04-24 23:05:41 +0900

    add sample route and controller

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const { readdirSync } = require("fs");
 
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("welcome from home");
-});
-app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
-});
+readdirSync("./router").map((route) =>
+    app.use("/", require(`./router/${route}`))
+);
+
 app.listen(8000, () => {
-  console.log("server is lestining...");
+    console.log("server is lestining...");
 });
