e08980586fad61b3cf7a0b83a0d952c8d6b05452
Armanur Rahman <mubeenarman19@gmail.com>
2023-04-24 23:58:02 +0900

    add database connection

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const { readdirSync } = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 readdirSync("./router").map((route) =>
     app.use("/", require(`./router/${route}`))
 );
+
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log("connect to database");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
