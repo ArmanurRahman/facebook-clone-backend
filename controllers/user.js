ce13d362a7b76aadc5930a7c0540359709ad080b
Armanur Rahman <mubeenarman19@gmail.com>
2023-04-25 23:43:47 +0900

    add user validate and generate username dynamically

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,65 @@
 const User = require("../model/user");
+const validator = require("../helpers/validate");
+const crypt = require("bcrypt");
 
 exports.register = async (req, res) => {
-    const {
-        firstName,
-        lastName,
-        userName,
-        email,
-        password,
-        bYear,
-        bMonth,
-        bDay,
-        gender,
-    } = req.body;
-    const user = await new User({
-        firstName,
-        lastName,
-        userName,
-        email,
-        password,
-        bYear,
-        bMonth,
-        bDay,
-        gender,
-    }).save();
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+        } = req.body;
+        if (!validator.emailValidate(email)) {
+            return res.status(400).json({ message: "Invalid email address" });
+        }
 
-    res.json(user);
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({ message: "Email already exist" });
+        }
+
+        if (!validator.lengthCheck(firstName, 2, 30)) {
+            return res
+                .status(400)
+                .json({ message: "first name must be 2 ~ 30 character" });
+        }
+
+        if (!validator.lengthCheck(lastName, 2, 30)) {
+            return res
+                .status(400)
+                .json({ message: "last name must be 2 ~ 30 character" });
+        }
+
+        if (!validator.lengthCheck(password, 6, 30)) {
+            return res
+                .status(400)
+                .json({ message: "password must be 2 ~ 30 character" });
+        }
+
+        const hashPass = await crypt.hash(password, 12);
+
+        const userName = await validator.usernameValidation(
+            `${firstName}${lastName}`
+        );
+        const user = await new User({
+            firstName,
+            lastName,
+            userName,
+            email,
+            password: hashPass,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+        }).save();
+
+        res.json(user);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 };
---
+++ helpers/validate.js
@@ -0,0 +1,28 @@
+const User = require("../model/user");
+
+exports.emailValidate = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+exports.lengthCheck = (string, min, max) => {
+    return !(string.length > max || string.length < min);
+};
+
+exports.usernameValidation = async (userName) => {
+    let isContinue = true;
+    do {
+        const existusername = await User.findOne({ userName });
+        if (existusername) {
+            userName += (+new Date() + Math.random())
+                .toString()
+                .substring(0, 1);
+        } else {
+            isContinue = false;
+        }
+    } while (isContinue);
+    return userName;
+};
